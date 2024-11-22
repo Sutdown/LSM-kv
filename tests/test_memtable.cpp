3ebfe7bd9cc3218e52644ed2af3cf2692fd736b5
Sutdown <2155197641@qq.com>
2024-11-22 21:46:19 +0800

    debug

---
+++ tests/test_memtable.cpp
@@ -0,0 +1,111 @@
+//
+// Created by qianyy on 2023/1/26.
+//
+#include <iostream>
+#include <ctime>
+#include <memory>
+#include <string>
+#include <gtest/gtest.h>
+#include "memtable/memtable.h"
+#include "memtable/memtable_iterator.h"
+#include "memory/allocate.h"
+
+namespace lsmkv::unittest {
+    TEST(MemTable, Insert__Add) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        auto mem_table = std::make_shared<MemTable>(alloc);
+
+        constexpr int N = 1234;
+        for (int i = 0; i < N; ++i) {
+            auto key = "key_" + std::to_string(i);
+            auto val = "val_" + std::to_string(i);
+            mem_table->Add(key, val);
+        }
+        for (int i = 0; i < N; ++i) {
+            auto key = "key_" + std::to_string(i);
+            auto val = "val_" + std::to_string(i);
+            EXPECT_EQ(mem_table->Get(key), val);
+        }
+    }
+
+    // 插入重复值
+    TEST(MemTable, Insert__add_update) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        auto mem_table = std::make_shared<MemTable>(alloc);
+
+        srand(time(nullptr));
+        constexpr int N = 1234;
+
+        for (int i = 0; i < N; ++i) {
+            auto key = "key_" + std::to_string(i);
+            auto val = "val_" + std::to_string(i);
+            mem_table->Add(key, val);
+            // 随机插入重复值
+            if (rand() & 1) {
+                auto duplicate_val = "duplicate_" + val;
+                mem_table->Update(key, duplicate_val);
+                EXPECT_EQ(mem_table->Get(key), duplicate_val);
+            } else {
+                EXPECT_EQ(mem_table->Get(key), val);
+            }
+        }
+    }
+
+    TEST(MemTable, Insert__Deletion) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        auto mem_table = std::make_shared<MemTable>(alloc);
+
+        srand(time(nullptr));
+        constexpr int N = 1234;
+
+        for (int i = 0; i < N; ++i) {
+            auto key = "key_" + std::to_string(i);
+            auto val = "val_" + std::to_string(i);
+            mem_table->Add(key, val);
+            // 随机插入重复值
+            if (rand() & 1) {
+                auto duplicate_val = "duplicate_" + val;
+                mem_table->Update(key, duplicate_val);
+                EXPECT_EQ(mem_table->Get(key), duplicate_val);
+            } else {
+                EXPECT_EQ(mem_table->Get(key), val);
+            }
+
+            // 随机删除
+            if (rand() & 1) {
+                mem_table->Delete(key);
+                EXPECT_EQ(mem_table->Get(key), "");
+            }
+        }
+    }
+
+    TEST(MemTable, MemTableIterator) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        auto mem_table = std::make_shared<MemTable>(alloc);
+
+        const int N = 1000;
+        // 构建插入数据
+        std::vector<std::string> data_key, data_value;
+        for (int i = 0; i < N; ++i) {
+            data_key.emplace_back("key_" + std::to_string(i));
+            data_value.emplace_back("value_" + std::to_string(i));
+        }
+
+        std::sort(data_key.begin(), data_key.end());
+        std::sort(data_value.begin(), data_value.end());
+
+        // 插入
+        for (int i = 0; i < N; ++i) {
+            mem_table->Add(data_key[i], data_value[i]);
+        }
+
+        auto iter = mem_table->NewIter();
+        iter->MoveToFirst();
+        // 测试迭代器
+        for (int i = 0; i < N; ++i) {
+            EXPECT_EQ(iter->key(), data_key[i]);
+            EXPECT_EQ(iter->value(), data_value[i]);
+            iter->Next();
+        }
+    }
+}
---
+++ tests/test_skiplist.cpp
@@ -0,0 +1,182 @@
+//
+// Created by qianyy on 2023/1/4.
+//
+#include <iostream>
+#include <ctime>
+#include <memory>
+#include <string>
+#include <gtest/gtest.h>
+#include "memtable/skiplist.h"
+#include "memory/allocate.h"
+
+namespace lsmkv::unittest {
+    TEST(skiplist, Insert) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        std::shared_ptr<SkipList<std::string, std::string>> skiplist =
+                std::make_shared<SkipList<std::string, std::string>>(alloc);
+        skiplist->Insert("1", "value_1");
+        skiplist->Insert("2", "value_2");
+    }
+
+    TEST(skiplist, Insert2) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        std::shared_ptr<SkipList<std::string, std::string>> skiplist =
+                std::make_shared<SkipList<std::string, std::string>>(alloc);
+        for (int i = 0; i < 100; ++i) {
+            skiplist->Insert(std::to_string(i), "value_" + std::to_string(i));
+        }
+    }
+
+    TEST(skiplist, Contains) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        std::shared_ptr<SkipList<std::string, std::string>> skiplist =
+                std::make_shared<SkipList<std::string, std::string>>(alloc);
+
+        skiplist->Insert("1", "value_1");
+        skiplist->Insert("3", "value_3");
+        skiplist->Insert("5", "value_5");
+        EXPECT_EQ(skiplist->Contains("1"), true);
+        EXPECT_EQ(skiplist->Contains("3"), true);
+        EXPECT_EQ(skiplist->Contains("5"), true);
+        EXPECT_EQ(skiplist->Contains("0"), false);
+        EXPECT_EQ(skiplist->Contains("2"), false);
+        EXPECT_EQ(skiplist->Contains("4"), false);
+        EXPECT_EQ(skiplist->Contains("6"), false);
+    }
+
+    TEST(skiplist, Contains2) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        std::shared_ptr<SkipList<std::string, std::string>> skiplist =
+                std::make_shared<SkipList<std::string, std::string>>(alloc);
+
+        const int N = 2000;
+        srand(time(0));
+        std::vector<std::string> yes;
+        std::vector<std::string> no;
+        for (int i = 0; i < N; ++i) {
+            if (rand() & 1) {
+                yes.push_back(std::to_string(i));
+            } else {
+                no.push_back(std::to_string(i));
+            }
+        }
+        for (const auto &ye: yes) {
+            skiplist->Insert(ye, "value_" + ye);
+        }
+
+        for (const auto &y: yes) {
+            EXPECT_EQ(skiplist->Contains(y), true);
+        }
+        for (const auto &n: no) {
+            EXPECT_EQ(skiplist->Contains(n), false);
+        }
+    }
+
+    TEST(skiplist, Delete) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        std::shared_ptr<SkipList<std::string, std::string>> skiplist =
+                std::make_shared<SkipList<std::string, std::string>>(alloc);
+
+        skiplist->Insert("1", "value_1");
+        skiplist->Insert("3", "value_3");
+        skiplist->Insert("5", "value_5");
+        EXPECT_EQ(skiplist->Contains("1"), true);
+        EXPECT_EQ(skiplist->Contains("3"), true);
+        EXPECT_EQ(skiplist->Contains("5"), true);
+
+        skiplist->Delete("1");
+        skiplist->Delete("3");
+        skiplist->Delete("5");
+        EXPECT_EQ(skiplist->Contains("1"), false);
+        EXPECT_EQ(skiplist->Contains("3"), false);
+        EXPECT_EQ(skiplist->Contains("5"), false);
+    }
+
+    TEST(skiplist, Delete2) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        std::shared_ptr<SkipList<std::string, std::string>> skiplist =
+                std::make_shared<SkipList<std::string, std::string>>(alloc);
+        const int N = 2000;
+        for (int i = 0; i < N; ++i) {
+            skiplist->Insert(std::to_string(i), "value_" + std::to_string(i));
+            int flag = rand() & 0x1;
+            if (flag == 1) {
+                skiplist->Delete(std::to_string(i));
+                EXPECT_EQ(skiplist->Contains(std::to_string(i)), false);
+            } else {
+                EXPECT_EQ(skiplist->Contains(std::to_string(i)), true);
+            }
+        }
+    }
+
+    TEST(skiplist, Get) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        std::shared_ptr<SkipList<std::string, std::string>> skiplist =
+                std::make_shared<SkipList<std::string, std::string>>(alloc);
+
+        skiplist->Insert("1", "value_1");
+        skiplist->Insert("3", "value_3");
+        skiplist->Insert("5", "value_5");
+
+        EXPECT_EQ(skiplist->Get("0"), std::nullopt);
+        EXPECT_EQ(skiplist->Get("1"), "value_1");
+        EXPECT_EQ(skiplist->Get("2"), std::nullopt);
+        EXPECT_EQ(skiplist->Get("3"), "value_3");
+        EXPECT_EQ(skiplist->Get("4"), std::nullopt);
+        EXPECT_EQ(skiplist->Get("5"), "value_5");
+    }
+
+    TEST(skiplist, Get2) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        std::shared_ptr<SkipList<std::string, std::string>> skiplist =
+                std::make_shared<SkipList<std::string, std::string>>(alloc);
+        const int N = 1234;
+        for (int i = 0; i < N; ++i) {
+            skiplist->Insert(std::to_string(i), "value_" + std::to_string(i));
+            if (i & 1) {
+                skiplist->Delete(std::to_string(i));
+            }
+        }
+        for (int i = 0; i < N; ++i) {
+            if (i & 1) {
+                EXPECT_EQ(skiplist->Get(std::to_string(i)), std::nullopt);
+            } else {
+                EXPECT_EQ(skiplist->Get(std::to_string(i)), "value_" + std::to_string(i));
+            }
+        }
+    }
+
+
+    TEST(skiplist, GetMemUsage_and_GetSize) {
+        auto alloc = std::make_shared<FreeListAllocate>();
+        std::shared_ptr<SkipList<std::string, std::string>> skiplist =
+                std::make_shared<SkipList<std::string, std::string>>(alloc);
+
+        EXPECT_EQ(skiplist->GetSize(), 0);
+        EXPECT_EQ(skiplist->GetMemUsage(), 0);
+
+        skiplist->Insert("1", "value_1");
+        EXPECT_EQ(skiplist->GetSize(), 1);
+        EXPECT_EQ(skiplist->GetMemUsage(), 8);
+
+        skiplist->Insert("3", "value_3");
+        EXPECT_EQ(skiplist->GetSize(), 2);
+        EXPECT_EQ(skiplist->GetMemUsage(), 16);
+
+        skiplist->Insert("5", "value_5");
+        EXPECT_EQ(skiplist->GetSize(), 3);
+        EXPECT_EQ(skiplist->GetMemUsage(), 24);
+
+        skiplist->Delete("1");
+        EXPECT_EQ(skiplist->GetSize(), 2);
+        EXPECT_EQ(skiplist->GetMemUsage(), 16);
+
+        skiplist->Delete("3");
+        EXPECT_EQ(skiplist->GetSize(), 1);
+        EXPECT_EQ(skiplist->GetMemUsage(), 8);
+
+        skiplist->Delete("5");
+        EXPECT_EQ(skiplist->GetSize(), 0);
+        EXPECT_EQ(skiplist->GetMemUsage(), 0);
+    }
+}
